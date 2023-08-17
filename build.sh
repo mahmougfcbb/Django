9874134bf43cb0f1890bb8e8dc8b119ca6c96634
mahmougfcbb <55944543+mahmougfcbb@users.noreply.github.com>
2023-08-17 15:40:25 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+# Exit on error
+set -e
 
-set -o errexit
+# Install Python package requirements
+pip install -r requirements.txt
+
+# Install project dependencies using Poetry
 poetry install
+
+# Upgrade pip in the virtual environment
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
