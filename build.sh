6c2fda34d697fe47ae0c3b05aead0aa1ae0d014e
mahmougfcbb <55944543+mahmougfcbb@users.noreply.github.com>
2023-08-17 12:59:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
+
 set -o errexit
-
-pip install -r requirements.txt
-
+poetry install
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 python manage.py collectstatic --no-input
 python manage.py migrate
